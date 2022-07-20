19a902297afb49a3e59841e5c739bc3faaeb0ce9
kong <kong08555@gmail.com>
2022-07-20 15:15:11 +0700

    change answer1

--- QuizGame.py
+++ QuizGame.py
@@ -10,13 +10,13 @@ print("Let's Start the Game :) ",name_player)
 
 score = 0
 
-answer = input(' UCL in 2022 what team meets what team in final\n team1 \n ')
-if answer.lower() == 'central processing unit':
+answer = input(' UCL in 2022 what team wins \n ')
+if answer.lower() == "real mardrid":
     print("Correct")
     score += 1
 else:
     print('Wrong')
-answer = input(' What is GPU stands for? \n ')
+answer = input(' team2 \n ')
 if answer.lower() == 'graphical processing unit':
     print("Correct")
     score += 1
